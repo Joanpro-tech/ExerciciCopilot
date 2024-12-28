c9224ed8432277c33b702a86e37775e3bd442374
joanpro-tech <jcarbonell@iespolitecnic.cat>
2024-12-28 16:54:29 +0100

    calendariSeleccio

--- app-api/src/app/date-keeper/date-keeper.component.ts
+++ app-api/src/app/date-keeper/date-keeper.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-date-keeper',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
   templateUrl: './date-keeper.component.html',
   styleUrls: ['./date-keeper.component.css']
 })
-export class DateKeeperComponent implements OnInit {
-  currentDate: Date = new Date();
-
-  ngOnInit() {
-    setInterval(() => {
-      this.currentDate = new Date();
-    }, 1000);
-  }
+export class DateKeeperComponent {
+  selectedDate: Date = new Date();
 }
--- app-api/src/main.ts
+++ app-api/src/main.ts
@@ -2,9 +2,23 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes), provideAnimationsAsync(),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatFormFieldModule,
+      MatInputModule
+     )
   ]
 }).catch(err => console.error(err));
